e5cd1de55b54186048617bd0798a5edba4754135
Jonatan Cantarino <jjcantarino@gmail.com>
2023-07-01 19:07:03 +0200

    Comment dynamodb

--- terraform/init-backend/output.tf
+++ terraform/init-backend/output.tf
@@ -3,10 +3,10 @@ output "state_bucket" {
   value       = module.remote_state.state_bucket.bucket
 }
 
-output "dynamodb_table" {
-  description = "The DynamoDB table to manage lock states"
-  value       = module.remote_state.dynamodb_table.table
-}
+#output "dynamodb_table" {
+#  description = "The DynamoDB table to manage lock states"
+#  value       = module.remote_state.dynamodb_table.table
+#}
 
 output "kms_key" {
   description = "The KMS customer master key to encrypt state buckets."
